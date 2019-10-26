a42fba40f55b0056064a8bfbdd3fe65fa3c9fdfe
deanroland <45372296+deanroland@users.noreply.github.com>
2019-10-25 21:12:23 -0700

    Implemented basic zombie movement

---
+++ Orcajam1/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour
+{
+    [SerializeField] private float Speed = 2f;
+
+    private static GameObject Player;
+    private CharacterController Controller;
+
+    private void Start()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
+        Controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        FacePlayer();
+        Controller.SimpleMove(transform.TransformDirection(Vector3.forward) * Speed);
+    }
+
+    private void FacePlayer()
+    {
+        transform.LookAt(new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z));
+    }
+
+}
