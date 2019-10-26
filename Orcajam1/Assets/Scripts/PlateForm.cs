cbfed585ae3e447018a4806359dd3ce3ba6e24da
xinluchen <1427355239@qq.com>
2019-10-25 20:57:13 -0700

    plateform

--- Orcajam1/Assets/Scripts/PlateForm.cs
+++ Orcajam1/Assets/Scripts/PlateForm.cs
@@ -4,11 +4,46 @@ using UnityEngine;
 
 public class PlateForm : MonoBehaviour
 {
-    [SerializeField] GameObject Tile;
+    [SerializeField] GameObject tile;
+    [SerializeField] GameObject parent1;
+    [SerializeField] GameObject parent2;
+    [SerializeField] GameObject parent3;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject[] floorOneTiles = new GameObject[400];
+        int writePos = 0;
+        for(int x = -10; x <= 10; x++)
+        {
+            for(int z = -10; z <= 10; z++)
+            {
+                floorOneTiles[writePos] = Instantiate(tile, new Vector3(x, 0, z), Quaternion.identity,parent1.transform);
+            }
+        }
+
+        GameObject[] floorTwoTiles = new GameObject[400];
+        writePos = 0;
+        for (int x = -10; x <= 10; x++)
+        {
+            for (int z = -10; z <= 10; z++)
+            {
+                floorTwoTiles[writePos] = Instantiate(tile, new Vector3(x, -5, z), Quaternion.identity, parent2.transform);
+            }
+        }
+
+        GameObject[] floorThreeTiles = new GameObject[400];
+        writePos = 0;
+        for (int x = -10; x <= 10; x++)
+        {
+            for (int z = -10; z <= 10; z++)
+            {
+                floorThreeTiles[writePos] = Instantiate(tile, new Vector3(x, -10, z), Quaternion.identity, parent3.transform);
+            }
+        }
+
     }
 
     // Update is called once per frame
