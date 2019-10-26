771eba3de3c8517d6e4b50e1463fd6d37dd87ef0
deanroland <45372296+deanroland@users.noreply.github.com>
2019-10-25 20:35:16 -0700

    Implemented horizontal

---
+++ Orcajam1/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    [SerializeField] private float Speed = 10f;
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+    }
+}
