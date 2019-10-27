1671bc088924bef879ac652a050fc9e7e8c241d2
icecreamin <icecremein@gmail.com>
2019-10-27 15:54:27 -0700

    play now turns when you are moving

--- Orcajam1/Assets/Scripts/ControlPlayer.cs
+++ Orcajam1/Assets/Scripts/ControlPlayer.cs
@@ -5,6 +5,7 @@ public class ControlPlayer : MonoBehaviour
     [SerializeField] private float Speed = 10f;
 
     private CharacterController Controller;
+	public Animator animator;
 
     private void Start()
     {
@@ -16,5 +17,11 @@ public class ControlPlayer : MonoBehaviour
         float xTranslation = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
         float zTranslation = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
         Controller.SimpleMove(new Vector3(xTranslation, 0, zTranslation) * Speed);
+		animator.SetFloat("Horizontal", xTranslation);
+		animator.SetFloat("Vertical", zTranslation);
     }
+	
+	void Animate(){
+		
+	}
 }
