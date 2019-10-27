e38bb74a3aef515615ff8197adabc75ad87a8100
Alex <alex116.dll@gmail.com>
2019-10-27 13:36:09 -0700

    making game over

---
+++ Orcajam1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] public Image healthDisplay;
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        healthDisplay.fillAmount = 1f;
+    }
+
+    private void Damage(float amount = 0.1f)
+    {
+        healthDisplay.fillAmount -= amount;
+        if (healthDisplay.fillAmount <= 0)
+        {
+            player.GetComponent<CharacterController>().enabled = false;
+        }
+
+    }
+}
