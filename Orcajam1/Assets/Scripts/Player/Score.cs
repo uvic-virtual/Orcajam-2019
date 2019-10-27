9339933d6af9b636519917bb84d9770ad28ce71f
Thomas Roberts <temersus@gmail.com>
2019-10-27 13:49:49 -0700

    Revert "making game over"
    This reverts commit e38bb74a3aef515615ff8197adabc75ad87a8100.

--- Orcajam1/Assets/Scripts/Player/Score.cs
+++ Orcajam1/Assets/Scripts/Player/Score.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
@@ -8,21 +9,32 @@ public class Score : MonoBehaviour
     [SerializeField] private Text display;
 
     private GameObject player;
+    private CharacterController controller;
+    private string[] highScores;
     private uint currScore;
 
     void Start()
     {
         player = GameObject.Find("Player");
+        controller = player.GetComponent<CharacterController>();
+        controller.enabled = false;
 
         display.text = "Score: 0";
         currScore = 0;
-
+        //read from file
+        string path = Application.dataPath + "/Scripts/Player/HighScores.txt";
+        StreamReader reader = new StreamReader(path);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        
+        highScores = content.Split('\n');
+        //print(highScores[0]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y < 5f && player.transform.position.y > -17)
+        if(player.transform.position.y < 5f)
         {
             UpdateScore();
         }     
@@ -34,8 +46,9 @@ public class Score : MonoBehaviour
         display.text = "Score: "+ currScore;
     }
 
-    public uint GetScore()
+    public void StartGame()
     {
-        return currScore;
+        Debug.Log("started");
+        controller.enabled = true;
     }
 }
