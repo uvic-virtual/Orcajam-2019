798f8e46c37560eb7bd22cd05f3247b87c5816aa
Alex <alex116.dll@gmail.com>
2019-10-26 17:13:26 -0700

    added score

---
+++ Orcajam1/Assets/Scripts/Player/Score.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private Text display;
+
+    private string[] highScores;
+    private uint currScore;
+
+    void Start()
+    {
+        display.text = "Score: 0";
+        currScore = 0;
+        //read from file
+        string path = Application.dataPath + "/Scripts/Player/HighScores.txt";
+        StreamReader reader = new StreamReader(path);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        highScores = content.Split('\n');
+        //print(highScores[0]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateScore();
+    }
+
+    private void updateScore()
+    {
+        currScore ++;
+        display.text = "Score: "+ currScore;
+    }
+}
