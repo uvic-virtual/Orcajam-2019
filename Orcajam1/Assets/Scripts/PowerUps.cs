89ccd6cb9e30643b28b8529b63f209f6c0f01fd6
xinluchen <1427355239@qq.com>
2019-10-27 17:44:30 -0700

    added power ups

--- Orcajam1/Assets/Scripts/PowerUps.cs
+++ Orcajam1/Assets/Scripts/PowerUps.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int platformSize = 8;
+    [SerializeField] private float spawnTime = 2f;
+    [SerializeField] private GameObject[] items;
+
+    private float timer;
+    private GameObject player;
     void Start()
     {
-        
+        timer = 0;
+        player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= spawnTime)
+        {
+            timer = 0f;
+            float height = player.transform.position.y;
+            if (height > 5f)
+                return;
+            float itemY = 0.5f;
+            if (height > -1f && height <= 5f)
+            {
+                itemY = 0.5f;
+            }
+            if (height > -6f && height <= -2f)
+            {
+                itemY = -4.5f;
+            }
+            if (height > -11f && height <= -7f)
+            {
+                itemY = -9.5f;
+            }
+            Spawn(new Vector3(RanNum(), itemY, RanNum()));
+        }
     }
+
+    private void Spawn(Vector3 pos)
+    {
+        int index = Random.Range(0, items.Length - 1);
+        GameObject clone = Instantiate(items[index], pos,Quaternion.identity);
+        Destroy(clone, 5f);
+    }
+
+    private int RanNum()
+    {
+        return Random.Range(-platformSize, platformSize);
+    }
+
+
 }
